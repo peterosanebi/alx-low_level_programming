6b0ad14328177b169aac350dc401aba1cae8343e
Osanebi Peter <osanebipeter@gmail.com>
2022-06-04 06:21:00 -0700

    using while loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main main code block
+ *
+ *Return: prints a to z in lower characters
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+		putchar(letter);
+		letter++;
+	return (0);
+
+}
