ef14030df1c6f91079720d6f39bc756e0494520b
Osanebi Peter <osanebipeter@gmail.com>
2022-06-08 14:38:23 -0700

    plus or minus

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - main function block
+ *
+ * @n: The number to be checked
+ *
+ * Return: return 1 if n is positive,
+ * return 0 if n is 0, and
+ * return -1 if n is less than 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
