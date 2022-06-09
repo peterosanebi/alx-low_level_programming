d1148d48ae67554dd90ac299aec2ccfcabcb90b0
Osanebi Peter <osanebipeter@gmail.com>
2022-06-09 13:31:15 -0700

    print 0 to 9 without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - main code block
+ */
+void print_most_numbers(void)
+{
+	int n = 48;
+
+	while (n < 58)
+	{
+		if (n != 50 && n != 52)
+			_putchar(n);
+		n++;
+	}
+	_putchar(10);
