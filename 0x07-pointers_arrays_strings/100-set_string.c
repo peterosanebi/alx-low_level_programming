afe4ade54b22708652ce85c0109c46b940293133
Osanebi Peter <osanebipeter@gmail.com>
2022-06-20 15:26:45 -0700

    set value to pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - main code block
+ * @s: pointer to change
+ * @to: string to change pointer to
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
