195141ce05f37453c5ee07a4d37035097630a2f1
Osanebi Peter <osanebipeter@gmail.com>
2022-06-22 14:35:56 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _sqrt - main code block
+ * @x: num
+ * @y: root
+ * Return: root of x
+ */
+int _sqrt(int x, int y)
+{
+	int sum;
+
+	sum = x - y;
+	if (sum < 0)
+		return (-(y / 2) - 1);
+	if (sum == 0)
+		return (1);
+	else
+		return (1 + _sqrt(sum, y + 2));
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (_sqrt(n, 1));
+}
