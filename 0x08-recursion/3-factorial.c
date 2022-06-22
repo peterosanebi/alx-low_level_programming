26638e71cfd9d0da1be2d3a6d10408e469c89079
Osanebi Peter <osanebipeter@gmail.com>
2022-06-22 14:28:31 -0700

    print factorial of n

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * factorial - main code block
+ *
+ *
+ * @n: parameter
+ *
+ * Return: n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		n = n * factorial(n - 1);
+		return (n);
+	}
+
+	return (0);
+}
