8ed0e4cd2234012d709b07175133ae75bd66deab
Osanebi Peter <osanebipeter@gmail.com>
2022-06-28 13:40:36 -0700

    print whatismyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main code block
+ *
+ * @argc: counter of arguments passed
+ * @argv: vector of strings with arguments passed
+ *
+ * Return: 0
+ *
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This readme is for the task on argc and argv task
